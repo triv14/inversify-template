ea2b288687804f7327c8ddfef3a3ae92f792ce1d
Ben Martinez <ben.martinez@clincierge.com>
2022-07-30 09:21:39 -0400

    Seed pets and purchases tables

--- src/db/seeds/seed.ts
+++ src/db/seeds/seed.ts
@@ -14,6 +14,32 @@ export const seed = async (knex: Knex) => {
       email: faker.internet.email(),
     });
   }
-  
   await knex("customers").insert(customers);
+
+  // pets
+  const pets = [];
+
+  for (let i = 0; i < 20; i++) {
+    pets.push({
+      id: uuidv4(),
+      name: faker.name.firstName(),
+      ownerId: faker.helpers.arrayElement(customers).id,
+    });
+  }
+
+  await knex("pets").insert(pets);
+
+  // purchases
+  const purchases = [];
+
+  for (let i = 0; i < 100; i++) {
+    purchases.push({
+      id: uuidv4(),
+      customerId: faker.helpers.arrayElement(customers).id,
+      date: faker.date.past(),
+      total: faker.commerce.price(),
+    });
+  }
+
+  await knex("purchases").insert(purchases);
 };
