3040a646e8ea7cdcfb9d6a578ccaf85c73030dfc
Ben Martinez <ben.martinez@clincierge.com>
2022-07-30 11:04:37 -0400

    Create PetsDAO

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { Container } from "inversify";
 // injections
 import Customer from "./model/Customer";
+import Pet from "./model/Pet";
 
 import setupDb from "./db/db-setup";
 setupDb();
@@ -12,5 +13,6 @@ const container = new Container({
 });
 
 container.bind("Customer").toConstantValue(Customer);
+container.bind("Pet").toConstantValue(Pet);
 
 export { container };
