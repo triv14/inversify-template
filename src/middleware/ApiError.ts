1c5b4cb812f0a812c9628e374ac75ce8eb4d6d17
Benjamin Martinez <ben.martinez@clincierge.com>
2023-01-30 09:44:12 -0500

    Add missing ApiError file

---
+++ src/middleware/ApiError.ts
@@ -0,0 +1,29 @@
+class ApiError extends Error {
+    code: number;
+  
+    message: string;
+  
+    constructor(code: number, message: string) {
+      super(message);
+      this.message = message;
+      this.code = code;
+    }
+  
+    static badRequest(msg: string) {
+      return new ApiError(400, msg);
+    }
+  
+    static unauthorized(msg: string) {
+      return new ApiError(401, msg);
+    }
+  
+    static notFound(msg: string) {
+      return new ApiError(404, msg);
+    }
+  
+    static internal(msg: string) {
+      return new ApiError(500, msg);
+    }
+  }
+  
+  export default ApiError;
